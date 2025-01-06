9b6ae090b26b72ba3700f5e63ffd5bac0fa4e8bd
Timothy Olisaeloka Ngonadi <timothyolisaeloka@gmail.com>
2025-01-06 22:42:46 +0100

    remove comments

--- src/main/java/com/techstack/api/infrastructure/service/impl/AccountServiceImpl.java
+++ src/main/java/com/techstack/api/infrastructure/service/impl/AccountServiceImpl.java
@@ -22,18 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.List;
 
-
-/**
- * 1. Develop JAVA springboot application using JDK 17 and maven. The application should
- * contain the following items.
- * a. Different types of http methods
- * b. Implement a search method with pagination
- * c. Log all the request and response, preferably using AspectJ
- * d. In memory DB
- * e. Sample query that joins 2 tables
- * f. Call external API (for ex, www.google.com) from one of the service class method
- * g. Unit test
- * */
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
